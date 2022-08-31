cbe8994ea74e2ac1226c7b3f1cd0add815f6a303
Lucas Miguel de Arruda <limirruda@gmail.com>
2022-08-31 19:07:12 -0300

    Atualização

---
+++ src/web/anunciar_vaga.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUÍRON</title>
+
+    <link rel="stylesheet" href="styles/theme.css">
+    <link rel="stylesheet" href="styles/anunciar_vaga.css">
+</head>
+<body>
+    <?php include('partials/header.php'); ?>
+
+    <div class="container">
+        <div class="header">
+            <a href="#">Voltar</a>
+            <h1>Anunciar Vaga</h1>
+
+            <div class="content">
+                <form>
+                    <h3>Preencha todos os campos</h3>
+
+                    Nome da matéria: <input type="text"> <br><br>
+                    Link de acesso no CPS: <input type="url"> <br><br>
+                    Descrição da vaga: <textarea></textarea> <br><br>
+                    Requisito da vaga: <textarea></textarea> <br><br>
+                    Carga horária semanal: <input type="number"> <br><br>
+                    Média salarial: <input type="number"> <br><br>
+
+                    <input type="submit" value="Anunciar Vaga">
+                </form>
+            </div>
+        </div>
+
+        <div class="submit">
+            <h1>Ok</h1>
+        </div>
+    </div>
+</body>
+</html>
