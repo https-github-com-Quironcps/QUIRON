3e221b5265053671e913b8069e8e0316d6c3999f
Lucas Miguel de Arruda <limirruda@gmail.com>
2022-08-31 17:59:40 -0300

    Atualização

--- src/web/index.php
+++ src/web/index.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -6,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QUIRON</title>
 
-    <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/index_light.css">
 </head>
 <body>
 <div class="hero">
     <nav>
-        <img src="images/arco.png" class="logo" alt="Logo">
+        <img src="images/logos/arco-e-texto-dark.png" class="logo" alt="Logo">
         <ul>
             <li><a class="ab" href="#">Entrar</a></li>
             <li><a class="ab" href="#">Cadastrar-se</a></li>
@@ -21,31 +20,23 @@
 
 
     <div class="container">
-        <img src="images/592367.jpg" class='fundo_img'>
         <img src="images/logos/arco-light.png" class='fundo_logo'>
         <div class='fundo_texto'>
+            <br>
             <h1>O futuro emprego na palma das mãos</h1>
             <br>
             <p>Uma proposta de avanço para a empregabilidade de docentes nas instituições de ensino do Centro Paula Souza (CPS)</p>
             <br>
-            <button class='fundo_botao'>ENTRAR</button>
+            <button class='fundo_botao'>CONFIRA</button>
         </div>
         
-        <div class='div1'>
-            <br><br>
-            <img src="images/logo-html-5-256.png" class='img-marca'>
-            <img src="images/logo-css-3-256.png" class='img-marca'>
-            <img src="images/logo-javascript-256.png" class='img-marca'>
-            <img src="images/logo-php-256.png" class='img-marca'>
-            <img src="images/mysql-256.png" class='img-marca'>
-        </div>
-<br>
-        <hr>
-
-        <div class='div3'>
-            <h1>Footer</h1>
-            
+        <div>
+            <div>
+                
+            </div>
         </div>
     </div>
+
+
 </body>
 </html>
