36ff5b6c6183b28022f5f519f68d38d5b3be6cb0
Gabriela5652 <74539725+Gabriela5652@users.noreply.github.com>
2022-09-01 18:45:47 -0300

    a

--- src/web/partials/footer.php
+++ src/web/partials/footer.php
@@ -1,46 +1,41 @@
 <!DOCTYPE html>
-<html lang="pt=br">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="stylesheet" href="styles/footer.css">
 
-     <!-- links para os ícones -->
+    <!-- links para os ícones -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-
+    
     <title>FOOTER</title>
 </head>
 <body>
     <section class="footer">
         <div class="social">
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-github"></i></a>
-            <a href=""><i class="bi bi-envelope"></i></a>
+            <a href="#"><i class="bi bi-instagram"></i></a>
+            <a href="#"><i class="bi bi-github"></i></a>
+            <a href="#"><i class="bi bi-envelope"></i></a>
         </div>
 
         <ul class="list">
             <li>
                 <a href="#">Home</a>
             </li>
-
-            <li>
-                <a href="#">Fale conosco</a>
-            </li>
-
             <li>
                 <a href="#">Sobre</a>
+            </li>
+            <li>
+                <a href="#">Contato</a>
             </li>
         </ul>
 
         <p class="copyright">
-            Copyright©Quíron
+            Direitos reservados | Quíron © 2022
         </p>
-
-    </section>  
-
+    </section>
 </body>
 </html>
