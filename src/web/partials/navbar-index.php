9388a3ffeff43fe249f2bb688e3a402db6575e2d
Lucas Miguel de Arruda <limirruda@gmail.com>
2022-09-02 18:41:51 -0300

    Atualização NavBar Index

---
+++ src/web/partials/navbar-index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUIRON</title>
+
+    <link rel="stylesheet" href="styles/navbar-index.css">
+</head>
+<body>
+    <div class="navbar-indice">
+        <nav>
+            <img src="images/logos/arco-e-texto-dark.png" class="logo" alt="Logo">
+            <ul>
+                <li><a class="ab" href="#">Entrar</a></li>
+                <li><a class="ab" href="#">Cadastrar-se</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
