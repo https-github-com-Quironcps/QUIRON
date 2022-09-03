30eaf6fa54b4d7b783b58c1965a96ea551f9b893
Gabriela5652 <74539725+Gabriela5652@users.noreply.github.com>
2022-09-03 18:07:41 -0300

    footer

--- src/web/partials/footer.php
+++ src/web/partials/footer.php
@@ -5,12 +5,80 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="styles/footer.css">
-
     <!-- links para os ícones -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    
+    <!-- css interno -->
+    <style>
+
+    .footer{
+        padding: 40px 0;
+        background: rgba(54, 54, 54, 0.8);;
+    }
+
+    .footer .social{
+        text-align: center;
+        padding-bottom: 25px;
+    }
+
+    .footer .social a{
+        font-size: 24px;
+        color: inherit;
+        width: 40px;
+        height: 40px;
+        line-height: 38px;
+        display: inline-block;
+        text-align: center;
+        margin: 0 8px;
+        padding-top: 5px;
+        color: #363636;
+        opacity: 0.76;
+    }
+
+    .footer .social a:hover{
+        opacity: 1.0;   
+        color: 	#A9A9A9;
+        margin: 0 25px 0;
+        transition: all 0.8s ease;
+    }
+
+    .footer ul{
+        margin-top: 0;
+        padding: 0;
+        list-style: none;
+        font-size: 18px;
+        line-height: 1.6;
+        margin-bottom: 0;
+        text-align: center;
+    }
+
+    .footer ul li a{
+        color: inherit;
+        text-decoration: none;
+        opacity: 0.8;
+        color: #D3D3D3;
+    }
+
+    .footer ul li{
+        display: inline-block;
+        padding: 0 15px;
+    }
+
+    .footer ul li a:hover{
+        color: #363636;
+        transition: all 0.5s ease;
+    }
+
+    .footer .copyright{
+        margin-top: 15px;
+        text-align: center;
+        font-size: 13px;
+        color: #A9A9A9;
+        
+    }
+    </style>
 
     <title>FOOTER</title>
 </head>
