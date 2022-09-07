1f3098e290cec43140647605c1f2db37fa487142
MatheusPagaime17 <101603566+MatheusPagaime17@users.noreply.github.com>
2022-09-07 16:51:28 -0300

    a

---
+++ src/web/cadastrar_professor.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUIRON</title>
+
+
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/anunciar_vaga.css">
+
+
+</head>
+<body>
+   <?php include('partials/navbar-index.php'); ?>
+   <br>
+   <br>
+<center>
+    <div class="container1">
+        <div class="header">
+            <h1 id="title">Comece criando a sua conta na Quíron</h1>
+        </div>
+<br>
+<br>
+        <div class="div-container">
+            <div class="div-sub-container">
+                <form class="form">
+                    <label for="name">Nome:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">CPF:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">RG:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">Data de Nascimento:</label><br>
+                    <input class='input-textarea' type="date" <br><br><br>
+
+                    <label for="name">Email:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">Senha:</label><br>
+                    <input class='input-textarea' type="password"> <br><br>
+
+                    <label for="name">Confirmar Senha:</label><br>
+                    <input class='input-textarea' type="password"> <br><br>
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+</center>
+<br>
+
+</body>
+</html>
