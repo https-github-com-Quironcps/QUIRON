7361d460bc44e3e166729439e9b8d06efb5b706e
MatheusPagaime17 <101603566+MatheusPagaime17@users.noreply.github.com>
2022-09-10 22:00:25 -0300

    a

---
+++ src/web/esqueci_senha.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUIRON</title>
+
+
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/anunciar_vaga.css">
+
+
+</head>
+<body>
+<?php include('partials/navbar-index.php'); ?>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+ <center>
+   <div class="container1">
+        <div class="header">
+            <h1 id="title">Confirme seu e-mail para redefinir sua senha</h1>
+        </div>
+
+<form class="form">
+                    <label for="name">E-mail:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <input type="submit" class='botao-001' value="Enviar">
+</div>     
+<center>
+
+
+            
+</body>
+</html>
