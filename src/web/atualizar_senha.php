4a7d96c37f953f817a8bffb65f64ad213f373de2
MatheusPagaime17 <101603566+MatheusPagaime17@users.noreply.github.com>
2022-09-10 22:13:25 -0300

    a

---
+++ src/web/atualizar_senha.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUIRON</title>
+
+
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/anunciar_vaga.css">
+
+
+</head>
+<body>
+<?php include('partials/navbar-index.php'); ?>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+ <center>
+   <div class="container1">
+        <div class="header">
+            <h1 id="title">Insira sua nova senha</h1>
+        </div> <br>
+
+<form class="form">
+                    <label for="name">Nova Senha:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">Confirmar Senha:</label><br>
+                    <input class='input-textarea' type="text"> <br><br><br>
+
+                    <input type="submit" class='botao-001' value="Atualizar Senha">
+</div>     
+<center>
+
+
+            
+</body>
+</html>
