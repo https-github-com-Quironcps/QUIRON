9c7d3d59cf682cb69f1799d54cfd6fce6d05aeef
MatheusPagaime17 <101603566+MatheusPagaime17@users.noreply.github.com>
2022-09-10 21:41:24 -0300

    a

---
+++ src/web/cadastrar_escola.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUIRON</title>
+
+
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/anunciar_vaga.css">
+
+
+</head>
+<body>
+
+<center>
+    <div class="container1">
+        <div class="header">
+            <h1 id="title">Cadastre a sua Instituição</h1>
+        </div>
+<br>
+<br>
+        <div class="div-container">
+            <div class="div-sub-container">
+                <form class="form">
+                    <label for="name">Nome da Instituição (ETEC/FATEC):</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">Endereço:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">N°:</label> <br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">Bairro:</label><br>
+                    <input class='input-textarea' type="text" <br><br><br>
+
+                    <label for="name">CEP:</label> <br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">Telefone:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">Email:</label> <br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <input type="submit" class='botao-001' value="Cadastrar Escola">
+                        
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+</center>
+<br>
+
+</body>
+</html>
