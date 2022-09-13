adb8d003b7701296d95d46329903eb5ff10de9c8
MatheusPagaime17 <pagaime10@gmail.com>
2022-09-13 14:16:01 -0300

    a

--- src/web/entrar_escola.php
+++ src/web/entrar_escola.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="styles/styles_m/fonts.css">
 
     <link rel="stylesheet" href="styles/styles_m/owl.carousel.min.css">
 
