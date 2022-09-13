6a37ff32c56fbf8c399c379012f047835243110e
Lucas Miguel de Arruda <limirruda@gmail.com>
2022-09-13 06:45:19 -0300

    Atualização 2.4

--- src/web/perfil_escola.php
+++ src/web/perfil_escola.php
@@ -19,23 +19,19 @@
         <div class="div-alterar-img">
             <h3>Fotos de divulgação</h3>
 
-            <div class="sub-div-alterar-img2">
-                <img class="img-div-alterar-01" src="images/etec.jpeg">
-                <img class="img-div-alterar-01" src="images/etec.jpeg">
-                <img class="img-div-alterar-01" src="images/etec.jpeg">
-                <img class="img-div-alterar-01" src="images/etec.jpeg">
-
-                <img class="img-div-alterar-01" src="images/etec.jpeg">
-                <img class="img-div-alterar-01" src="images/etec.jpeg">
-                <img class="img-div-alterar-01" src="images/etec.jpeg">
-                <img class="img-div-alterar-01" src="images/etec.jpeg">
-
-            </div>
+            <img class="img-div-alterar-01" src="#"> <button id="submit-text-alterar" class="img1-excluir">Deletar</button><br>
+            <img class="img-div-alterar-01" src="#"> <button id="submit-text-alterar" class="img2-excluir">Deletar</button><br>
+            <img class="img-div-alterar-01" src="#"> <button id="submit-text-alterar" class="img3-excluir">Deletar</button><br>              
+            <img class="img-div-alterar-01" src="#"> <button id="submit-text-alterar" class="img4-excluir">Deletar</button><br>
+            <img class="img-div-alterar-01" src="#"> <button id="submit-text-alterar" class="img5-excluir">Deletar</button><br>                
+            <img class="img-div-alterar-01" src="#"> <button id="submit-text-alterar" class="img6-excluir">Deletar</button><br>
+            <img class="img-div-alterar-01" src="#"> <button id="submit-text-alterar" class="img7-excluir">Deletar</button><br>                
+            <img class="img-div-alterar-01" src="#"> <button id="submit-text-alterar" class="img8-excluir">Deletar</button><br>       
         </div>
 
         <div class="div-alterar-texto">
             <form action="" class="div-alterar-form">
-
+<br>
                 <label class="form-cursos">Cursos: </label><textarea class="input-cursos" type="text" id="input-text-alterar2"></textarea><br>
                 
                 <label class="form-sobre">Sobre: </label><br><textarea type="text" class="input-sobre" id="input-text-alterar2"></textarea><br>
