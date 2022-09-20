a0ddf6fcd6d7c3d9aed0c7789e9130b9ebcfa48a
aluno <aluno@LAB.ETEC187>
2022-09-20 14:11:33 -0300

    botoes

--- src/web/cadastrar_professor.php
+++ src/web/cadastrar_professor.php
@@ -56,8 +56,6 @@
                     <label for="name">Senha:</label><br>
                     <input class='input-textarea' type="password"> <br><br><br>
 
-                    <div class="linhaMeio"></div>
-                    
                     <input type="submit" class='botao-001' value="Cadastrar">
                         <br><br><br>
 
