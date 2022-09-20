1cee93af43aa6e6e2946eb54c7da7cc0b7dab055
aluno <aluno@LAB.ETEC187>
2022-09-20 13:48:10 -0300

    Update cadastrar_professor.php

--- src/web/cadastrar_professor.php
+++ src/web/cadastrar_professor.php
@@ -26,13 +26,25 @@
         <div class="div-container">
             <div class="div-sub-container">
                 <form class="form">
-                    <label for="name">Nome:</label><br>
+                    <label for="name">Nome Completo:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">CEP:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">Endereço:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">N°:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">Bairro:</label><br>
+                    <input class='input-textarea' type="text"> <br><br>
+
+                    <label for="name">Telefone:</label><br>
                     <input class='input-textarea' type="text"> <br><br>
 
                     <label for="name">CPF:</label><br>
-                    <input class='input-textarea' type="text"> <br><br>
-
-                    <label for="name">RG:</label><br>
                     <input class='input-textarea' type="text"> <br><br>
 
                     <label for="name">Data de Nascimento:</label><br>
@@ -42,10 +54,8 @@
                     <input class='input-textarea' type="text"> <br><br>
 
                     <label for="name">Senha:</label><br>
-                    <input class='input-textarea' type="password"> <br><br>
-
-                    <label for="name">Confirmar Senha:</label><br>
                     <input class='input-textarea' type="password"> <br><br><br>
+
 
                     <input type="submit" class='botao-001' value="Cadastrar">
                         <br><br><br>
