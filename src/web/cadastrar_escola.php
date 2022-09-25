c38f0c34a58fac4c4e3480ac9856ffb39ae601fc
MatheusPagaime17 <101603566+MatheusPagaime17@users.noreply.github.com>
2022-09-25 18:54:25 -0300

    telas cadastro

--- src/web/cadastrar_escola.php
+++ src/web/cadastrar_escola.php
@@ -4,25 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QUIRON</title>
-
+    <title>QUÍRON</title>
 
     <link rel="stylesheet" href="styles/styles_l/style.css">
-    <link rel="stylesheet" href="styles/styles_m/cadastro.css">
-
-
+    <link rel="stylesheet" href="styles/styles_l/anunciar_vaga.css">
+    
 </head>
 <body>
+<?php include('partials/navbar-index.php'); ?>
+<br>
+
+
 
 <center>
     <div class="container1">
         <div class="header">
             <h1 id="title">Cadastre a sua Instituição</h1>
         </div>
-<br>
+
         <div class="div-container">
             <div class="div-sub-container">
-                <form class="form">
+            <form class="form">
                     <label for="name">Nome da Instituição (ETEC/FATEC):</label><br>
                     <input class='input-textarea' type="text"> <br><br>
 
@@ -49,16 +51,34 @@
 
                     <input type="submit" class='botao-001' value="Cadastrar Escola">
                         <br><br><br>
-
-
+                </form>
             </div>
 
-<center>
+            <div class="linhaMeio"></div>
 
+                <center>
+                <div class="div-sub-container-2">
+                    <div class="ab">
+                    </div>
+                    <div class="ab">
+                            <br>
+                        </form>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                        <img src="images/svgs/img_login.svg" class="img-anunciar">
+                    </div>
+                    <div class="ab">
+                    </div>
+                    
+                </center>
+            </div>
         </div>
     </div>
 </center>
 <br>
-
 </body>
 </html>
