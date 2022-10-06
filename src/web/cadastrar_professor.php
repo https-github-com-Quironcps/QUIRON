9aaf2b0129d4eccfc7d3eefe1bc74658ebed04b7
MatheusPagaime17 <101603566+MatheusPagaime17@users.noreply.github.com>
2022-10-06 19:25:04 -0300

    tela cadastro professor com erro

--- src/web/cadastrar_professor.php
+++ src/web/cadastrar_professor.php
@@ -1,4 +1,8 @@
+<?php
+//Incluir conexão
+include_once "server/PDO/conexao.php";
 
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br" class="light">
@@ -26,9 +30,61 @@
             <h1 id="title">Crie sua conta</h1>
         </div>
 
+
+        <?php
+//receber dados do form
+$dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+
+//verificar se o usuário clicou em "cadastrar"
+if(!empty($dados['submit'])) {
+    var_dump($dados);
+}
+try{
+    //query de cadastro ao banco
+    $query_professor = "INSERT INTO TB_PROFESSOR (
+    TB_PROFESSOR_NOME,
+    TB_PROFESSOR_CEP,
+    TB_PROFESSOR_ENDERECO, 
+    TB_PROFESSOR_END_NUM, 
+    TB_PROFESSOR_BAIRRO, 
+    TB_PROFESSOR_TELEFONE,
+    TB_PROFESSOR_CPF,
+    TB_PROFESSOR_NASCIMENTO,
+    TB_PROFESSOR_EMAIL,
+    TB_PROFESSOR_SENHA) VALUES (:TB_PROFESSOR_NOME, :TB_PROFESSOR_CEP, :TB_PROFESSOR_ENDERECO, :TB_PROFESSOR_END_NUM, :TB_PROFESSOR_BAIRRO, :TB_PROFESSOR_TELEFONE, :TB_PROFESSOR_CPF, :TB_PROFESSOR_NASCIMENTO, :TB_PROFESSOR_EMAIL, :TB_PROFESSOR_SENHA)";
+
+
+$cad_professor = $pdo->prepare($query_professor);
+$cad_professor->bindParam(':TB_PROFESSOR_NOME', $dados['nome-p']);
+$cad_professor->bindParam(':TB_PROFESSOR_CEP', $dados['cep-p']);
+$cad_professor->bindParam(':TB_PROFESSOR_ENDERECO', $dados['end-p']);
+$cad_professor->bindParam(':TB_PROFESSOR_END_NUM', $dados['num-p']);
+$cad_professor->bindParam(':TB_PROFESSOR_BAIRRO', $dados['bairro-p']);
+$cad_professor->bindParam(':TB_PROFESSOR_TELEFONE', $dados['tel-p']);
+$cad_professor->bindParam(':TB_PROFESSOR_CPF', $dados['cpf-p']);
+$cad_professor->bindParam(':TB_PROFESSOR_NASCIMENTO', $dados['data-p']);
+$cad_professor->bindParam(':TB_PROFESSOR_EMAIL', $dados['email-p']);
+$cad_professor->bindParam(':TB_PROFESSOR_SENHA', $dados['senha-p']);
+
+//executar query
+$cad_professor->execute();
+
+//acessar if ao cadastrar corretamente
+if($cad_professor->()){
+echo "<span>Cadastrado com Sucesso!</span><br><br>";
+unset($dados);
+}else{
+    echo "<span>Erro: Não foi possivel realizar o cadastro.</span><br><br>";
+}
+}
+
+?>
+
+
         <div class="div-container">
             <div class="div-sub-container">
-            <form action="cadastrar_professor.php" method="POST" class="form">
+            <form action="" method="POST" class="form">
                     <label for="name">Nome Completo:</label><br>
                     <input class='input-textarea' name="nome-p" id="nome-p" type="text"> <br><br>
 
