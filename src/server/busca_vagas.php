a2f34cb88f211e45557164dd67ee4079dd53c4c5
Lucas Miguel de Arruda <limirruda@gmail.com>
2022-10-06 09:14:17 -0300

    Index - funcionalidades - Filtros

--- src/server/busca_vagas.php
+++ src/server/busca_vagas.php
@@ -1,7 +1,7 @@
 <?php
 
 // Busca por CODIGO DA ETEC
-if (isset($_GET['cod']) >= 1)
+if (!empty($_GET['cod']))
 {
     $cod = trim($_GET['cod']);
 
@@ -21,63 +21,154 @@ if (isset($_GET['cod']) >= 1)
 }
 
 elseif (isset($_GET['localizacao'], $_GET['grade'], $_GET['faixa'])
+&& !empty($_GET['localizacao'] && $_GET['grade'] && $_GET['faixa']))
+{
+    $localizacao = trim($_GET['localizacao']);
+    $faixa = trim($_GET['faixa']);
+    $grade = trim($_GET['grade']);
+
+    $sth = $pdo->prepare('SELECT 
+    TB_VAGA.TB_VAGA_MATERIA,
+    TB_INSTITUICAO.TB_INSTITUICAO_IMG_USER as Foto,
+    TB_INSTITUICAO.TB_INSTITUICAO_NOME as Nome
+    FROM `TB_VAGA` 
+    INNER JOIN `TB_INSTITUICAO` 
+    ON TB_VAGA.FK_INSTITUICAO = TB_INSTITUICAO.TB_INSTITUICAO_ID
+    WHERE `TB_VAGA_GRADE` LIKE :grade 
+    AND `TB_VAGA_FAIXA_SALARIAL` LIKE :faixa 
+    AND `TB_INSTITUICAO_CEP` LIKE :cep ');
+    
+    $sth->bindParam(':grade', $grade, PDO::PARAM_STR);
+    $sth->bindParam(':cep', $localizacao, PDO::PARAM_STR);
+    $sth->bindParam(':faixa', $faixa, PDO::PARAM_STR);
+    $sth->execute();
+    
+    $resultados = $sth->fetchAll(PDO::FETCH_ASSOC);
+}
+
+elseif (isset($_GET['localizacao'], $_GET['grade'], $_GET['faixa'])
 && !empty($_GET['localizacao'])){
     if (!empty($_GET['grade'])){
-        echo 'loca e grade';
+        $localizacao = trim($_GET['localizacao']);
+        $grade = trim($_GET['grade']);
+
+        $sth = $pdo->prepare('SELECT 
+        TB_VAGA.TB_VAGA_MATERIA,
+        TB_INSTITUICAO.TB_INSTITUICAO_IMG_USER as Foto,
+        TB_INSTITUICAO.TB_INSTITUICAO_NOME as Nome
+        FROM `TB_VAGA` 
+        INNER JOIN `TB_INSTITUICAO` 
+        ON TB_VAGA.FK_INSTITUICAO = TB_INSTITUICAO.TB_INSTITUICAO_ID
+        WHERE `TB_VAGA_GRADE` LIKE :grade 
+        AND `TB_INSTITUICAO_CEP` LIKE :cep ');
+
+        $sth->bindParam(':grade', $grade, PDO::PARAM_STR);
+        $sth->bindParam(':cep', $localizacao, PDO::PARAM_STR);
+        $sth->execute();
+
+        $resultados = $sth->fetchAll(PDO::FETCH_ASSOC);
     }
 
     elseif (!empty($_GET['faixa'])){
-        echo 'loca e faixa';
+        $localizacao = trim($_GET['localizacao']);
+        $faixa = trim($_GET['faixa']);
+
+        $sth = $pdo->prepare('SELECT 
+        TB_VAGA.TB_VAGA_MATERIA,
+        TB_INSTITUICAO.TB_INSTITUICAO_IMG_USER as Foto,
+        TB_INSTITUICAO.TB_INSTITUICAO_NOME as Nome
+        FROM `TB_VAGA` 
+        INNER JOIN `TB_INSTITUICAO` 
+        ON TB_VAGA.FK_INSTITUICAO = TB_INSTITUICAO.TB_INSTITUICAO_ID
+        WHERE `TB_VAGA_FAIXA_SALARIAL` LIKE :faixa 
+        AND `TB_INSTITUICAO_CEP` LIKE :cep ');
+
+        $sth->bindParam(':faixa', $faixa, PDO::PARAM_STR);
+        $sth->bindParam(':cep', $localizacao, PDO::PARAM_STR);
+        $sth->execute();
+
+        $resultados = $sth->fetchAll(PDO::FETCH_ASSOC);
     }
 
     else {
-        echo 'so localizacao';
+        $localizacao = trim($_GET['localizacao']);
+
+        $sth = $pdo->prepare('SELECT 
+        TB_VAGA.TB_VAGA_MATERIA,
+        TB_INSTITUICAO.TB_INSTITUICAO_IMG_USER as Foto,
+        TB_INSTITUICAO.TB_INSTITUICAO_NOME as Nome
+        FROM `TB_VAGA` 
+        INNER JOIN `TB_INSTITUICAO` 
+        ON TB_VAGA.FK_INSTITUICAO = TB_INSTITUICAO.TB_INSTITUICAO_ID
+        WHERE `TB_INSTITUICAO_CEP` LIKE :cep ');
+
+        $sth->bindParam(':cep', $localizacao, PDO::PARAM_STR);
+        $sth->execute();
+
+        $resultados = $sth->fetchAll(PDO::FETCH_ASSOC);
     }
 }
 
+elseif (isset($_GET['localizacao'], $_GET['grade'], $_GET['faixa'])
+&& !empty($_GET['grade'])){
+    if (!empty($_GET['faixa'])){
+        $faixa = trim($_GET['faixa']);
+        $grade = trim($_GET['grade']);
 
-// elseif (isset($GET['localizacao']) != ''){
-//     $localizacao = trim($_GET['localizacao']);
+        $sth = $pdo->prepare('SELECT 
+        TB_VAGA.TB_VAGA_MATERIA,
+        TB_INSTITUICAO.TB_INSTITUICAO_IMG_USER as Foto,
+        TB_INSTITUICAO.TB_INSTITUICAO_NOME as Nome
+        FROM `TB_VAGA` 
+        INNER JOIN `TB_INSTITUICAO` 
+        ON TB_VAGA.FK_INSTITUICAO = TB_INSTITUICAO.TB_INSTITUICAO_ID
+        WHERE `TB_VAGA_GRADE` LIKE :grade 
+        AND `TB_VAGA_FAIXA_SALARIAL` LIKE :faixa');
+        
+        $sth->bindParam(':grade', $grade, PDO::PARAM_STR);
+        $sth->bindParam(':faixa', $faixa, PDO::PARAM_STR);
+        $sth->execute();
+        
+        $resultados = $sth->fetchAll(PDO::FETCH_ASSOC);
+    }
 
-//     if (isset($_GET['faixa'])){
-//         $faixa = trim($_GET['faixa']);
+    else {
+        $grade = trim($_GET['grade']);
 
-//         if (isset($GET['grade'])){
-//             $grade = trim($_GET['grade']);
+        $sth = $pdo->prepare('SELECT 
+        TB_VAGA.TB_VAGA_MATERIA,
+        TB_INSTITUICAO.TB_INSTITUICAO_IMG_USER as Foto,
+        TB_INSTITUICAO.TB_INSTITUICAO_NOME as Nome
+        FROM `TB_VAGA` 
+        INNER JOIN `TB_INSTITUICAO` 
+        ON TB_VAGA.FK_INSTITUICAO = TB_INSTITUICAO.TB_INSTITUICAO_ID
+        WHERE `TB_VAGA_GRADE` LIKE :grade ');
 
-//             $sth = $pdo->prepare('SELECT * FROM `TB_VAGA` 
-//             INNER JOIN `TB_INSTITUICAO` 
-//             ON TB_VAGA.FK_INSTITUICAO = TB_INSTITUICAO.TB_INSTITUICAO_ID
-//             WHERE `TB_VAGA_GRADE` LIKE :grade 
-//             AND `TB_VAGA_FAIXA_SALARIAL` LIKE :faixa 
-//             AND `TB_INSTITUICAO_CEP` LIKE :cep ');
-            
-//             $sth->bindParam(':grade', $grade, PDO::PARAM_STR);
-//         }
+        $sth->bindParam(':grade', $grade, PDO::PARAM_STR);
+        $sth->execute();
 
-//         else {
-//             $sth = $pdo->prepare('SELECT * FROM `TB_VAGA` 
-//             INNER JOIN `TB_INSTITUICAO` 
-//             ON TB_VAGA.FK_INSTITUICAO = TB_INSTITUICAO.TB_INSTITUICAO_ID
-//             WHERE `TB_VAGA_FAIXA_SALARIAL` LIKE :faixa 
-//             AND `TB_INSTITUICAO_CEP` LIKE :cep ');
-//         }
+        $resultados = $sth->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
 
-//         $sth->bindParam(':faixa', $faixa, PDO::PARAM_STR);
-//     }
+elseif (isset($_GET['localizacao'], $_GET['grade'], $_GET['faixa'])
+&& !empty($_GET['faixa'])){
+    $faixa = trim($_GET['faixa']);
 
-//     else {
-//         $sth = $pdo->prepare('SELECT * FROM `TB_VAGA` 
-//         INNER JOIN `TB_INSTITUICAO` 
-//         ON TB_VAGA.FK_INSTITUICAO = TB_INSTITUICAO.TB_INSTITUICAO_ID
-//         WHERE `TB_INSTITUICAO_CEP` LIKE :cep ');
-//     }
+    $sth = $pdo->prepare('SELECT 
+    TB_VAGA.TB_VAGA_MATERIA,
+    TB_INSTITUICAO.TB_INSTITUICAO_IMG_USER as Foto,
+    TB_INSTITUICAO.TB_INSTITUICAO_NOME as Nome
+    FROM `TB_VAGA` 
+    INNER JOIN `TB_INSTITUICAO` 
+    ON TB_VAGA.FK_INSTITUICAO = TB_INSTITUICAO.TB_INSTITUICAO_ID
+    WHERE `TB_VAGA_FAIXA_SALARIAL` LIKE :faixa ');
 
-//     $sth->bindParam(':cep', $localizacao, PDO::PARAM_STR);
-//     $sth->execute();
-    
-//     $resultados = $sth->fetchAll(PDO::FETCH_ASSOC);
-// }
+    $sth->bindParam(':faixa', $faixa, PDO::PARAM_STR);
+    $sth->execute();
+
+    $resultados = $sth->fetchAll(PDO::FETCH_ASSOC);
+}
 
 else
 {
@@ -92,6 +183,4 @@ else
 
     $resultados = $sth->fetchAll(PDO::FETCH_ASSOC);
 }
-    
-
 ?>
