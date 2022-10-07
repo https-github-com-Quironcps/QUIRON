27640555c44ef465307ed62d50c51eb44feee590
Gabriela5652 <74539725+Gabriela5652@users.noreply.github.com>
2022-10-06 21:44:38 -0300

    dark-e-navbar

--- src/web/partials/header-inicio.php
+++ src/web/partials/header-inicio.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br" class="light">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <script src="../web/scripts/modo-dark.js"></script>
+
+    <link rel="stylesheet" href="../styles/navbar.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="scripts/ImagenError.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav class="nav">
+        <div class="container">
+        
+        </div>
+        </nav>
+    </header>
+</body>
+</html>
--- src/web/partials/header.php
+++ src/web/partials/header.php
@@ -3,7 +3,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br" class="light">
+<html lang="pt-br" class="dark">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
