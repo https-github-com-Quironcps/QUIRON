66904a3cbd4c1cc7f5efbf93a1efef674da53754
Gabriela5652 <74539725+Gabriela5652@users.noreply.github.com>
2022-10-08 19:44:33 -0300

    navbar-professor

--- src/web/partials/header-professor.php
+++ src/web/partials/header-professor.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="../styles/navbar.css">
+    <link rel="stylesheet" href="../styles/navbar-professor.css">
     <link rel="stylesheet" href="../styles/theme.css">
 
     <script src="../web/scripts/modo-dark.js"></script>
