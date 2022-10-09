7c8f7c3c607505f2fad49878a7571f3b861ab017
Gabriela5652 <74539725+Gabriela5652@users.noreply.github.com>
2022-10-09 10:43:56 -0300

    atualizações-front

--- src/web/visualizar_vaga.php
+++ src/web/visualizar_vaga.php
@@ -66,6 +66,14 @@
              </h5>
             </div>
 
+            <!-- <h5 class="title02">Requisitos da Vaga</h5>
+
+            <div class="desc-vaga">
+             <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui oficia deserunt mollit anim id est laborum.
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui oficia deserunt mollit anim id est laborum.
+             </h5>
+            </div> -->
+
             <!-- requisitos da vaga -->
             <!-- carga horária semanal -->
             <!-- média salarial -->
