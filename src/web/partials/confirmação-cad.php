f848f8ac9115b7ab81f2028b835ec5eb806e47e7
Gabriela5652 <74539725+Gabriela5652@users.noreply.github.com>
2022-10-14 22:03:48 -0300

    confirmação

---
+++ src/web/partials/confirmação-cad.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br" class="light">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../styles/theme.css">
+    <link rel="stylesheet" href="../styles/styles_g/style.css">
+
+    <title>QUIRON - CONFIRMAÇÃO</title>
+</head>
+<body>
+    <div class="principal">
+        <h5>hello</h5>
+    </div>
+</body>
+</html>
