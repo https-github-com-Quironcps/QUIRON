46dd207b9744810927a7e503789f849fb7007533
Gabriela5652 <74539725+Gabriela5652@users.noreply.github.com>
2022-10-25 15:04:34 -0300

    termos de uso

---
+++ src/web/partials/termos.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br" class="light">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUIRON - TERMOS DE USO</title>
+
+    
+
+    <link rel="icon" href="../web/images/logos/arco-dark-2.png">
+    <link rel="stylesheet" href="../styles/styles_g/termos.css">
+    <link rel="stylesheet" href="../styles/theme.css">
+
+</head>
+<body>
+    <center>
+    <div class="principal">
+        <i id="icon1" class="bi bi-arrow-left-short" onclick=""></i>
+        
+        <div class="termos">
+        <h3 class="t">TERMOS DE USO - Quíron</h3><br>
+        <h3 class="t">Esta política de Termos de Uso é válida a partir de Oct 2022.</h3><br>
+
+        <h4 class="s">Quíron, pessoa jurídica de direito privado descreve, através deste documento, as regras de uso do site
+                        quiron.com.br e qualquer outro site, loja ou aplicativo operado pelo proprietário.<br><br>
+                        Ao navegar neste website, consideramos que você está de acordo com os Termos de Uso abaixo.<br><br>
+                        Caso você não esteja de acordo com as condições deste contrato, pedimos que não faça mais uso deste website,
+                        muito menos cadastre-se ou envie os seus dados pessoais.<br><br>
+                        Se modificarmos nossos Termos de Uso, publicaremos o novo texto neste website, com a data de revisão
+                        atualizada. Podemos alterar este documento a qualquer momento. Caso haja alteração significativa nos termos
+                        deste contrato, podemos informá-lo por meio das informações de contato que tivermos em nosso banco de dados
+                        ou por meio de notificações.<br><br>
+                        A utilização deste website após as alterações significa que você aceitou os Termos de Uso revisados. Caso, após
+                        a leitura da versão revisada, você não esteja de acordo com seus termos, favor encerrar o seu acesso.<br>
+        </h4>
+
+        </div>
+    </div>
+    </center>
+</body>
+</html>
