2918d2afa2b60fd9abd45c5c25a3099d360a9e8c
Lucas Miguel de Arruda <limirruda@gmail.com>
2022-10-28 12:06:15 -0300

    Front - Cadastrar Escolas

--- src/server/cad_escola.php
+++ src/server/cad_escola.php
@@ -1,29 +1,30 @@
 <?php
 session_start();
 
-if(isset($_POST['submit']))
-{
-    if(isset($_POST['nome-e'],
-    $_POST['end-p'],
-    $_POST['num-e'],
-    $_POST['cidade-e'],
-    $_POST['cep-e'],
-    $_POST['tel-e'],
-    $_POST['email-e'],
-    $_POST['senha-e'],
-    $_POST['cod-e']) 
-    
-    && !empty($_POST['nome-e']) 
-    && !empty($_POST['end-e'])
-    && !empty($_POST['num-e'])
-    && !empty($_POST['cidade-e']) 
-    && !empty($_POST['cep-e'])
-    && !empty($_POST['tel-e'])
-    && !empty($_POST['email-e'])
-    && !empty($_POST['senha-e'])
-    && !empty($_POST['cod-e'])) 
+if (isset($_POST['submit'])) {
+    if (
+        isset(
+            $_POST['nome-e'],
+            $_POST['end-e'],
+            $_POST['num-e'],
+            $_POST['cidade-e'],
+            $_POST['cep-e'],
+            $_POST['tel-e'],
+            $_POST['email-e'],
+            $_POST['senha-e'],
+            $_POST['cod-e']
+        )
 
-    {
+        && !empty($_POST['nome-e'])
+        && !empty($_POST['end-e'])
+        && !empty($_POST['num-e'])
+        && !empty($_POST['cidade-e'])
+        && !empty($_POST['cep-e'])
+        && !empty($_POST['tel-e'])
+        && !empty($_POST['email-e'])
+        && !empty($_POST['senha-e'])
+        && !empty($_POST['cod-e'])
+    ) {
         $nome = trim($_POST['nome-e']);
         $endereco = trim($_POST['end-e']);
         $numero = trim($_POST['num-e']);
@@ -33,47 +34,43 @@ if(isset($_POST['submit']))
         $email = trim($_POST['email-e']);
         $senha = trim($_POST['senha-e']);
         $codigo = trim($_POST['cod-e']);
-        
-        $options = array("cost"=>4);
-        $hashsenha = password_hash($senha,PASSWORD_BCRYPT,$options);
+        $textarea = trim($_POST['imagem']);
 
-        if(filter_var($email, FILTER_VALIDATE_EMAIL))
-		{
+        $options = array("cost" => 4);
+        $hashsenha = password_hash($senha, PASSWORD_BCRYPT, $options);
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $sql = 'SELECT * FROM TB_INSTITUICAO where TB_INSTITUICAO_EMAIL = :TB_INSTITUICAO_EMAIL';
             $stmt = $pdo->prepare($sql);
-            $p = ['TB_INSTITUICAO_EMAIL'=>$email];
+            $p = ['TB_INSTITUICAO_EMAIL' => $email];
             $stmt->execute($p);
-            
-            if($stmt->rowCount() == 0)
-            {
-                $sql = "INSERT INTO TB_INSTITUICAO (TB_INSTITUICAO_NOME, TB_INSTITUICAO_ENDERECO, TB_INSTITUICAO_END_NUM, TB_INSTITUICAO_CIDADE, TB_INSTITUICAO_CEP, TB_INSTITUICAO_TELEFONE, TB_INSTITUICAO_EMAIL,`TB_INSTITUICAO_SENHA`,TB_INSTITUICAO_COD) 
-                values(:TB_INSTITUICAO_NOME,:TB_INSTITUICAO_ENDERECO,:TB_INSTITUICAO_END_NUM,:TB_INSTITUICAO_CIDADE,:TB_INSTITUICAO_CEP,:TB_INSTITUICAO_TELEFONE,:TB_INSTITUICAO_EMAIL,:TB_INSTITUICAO_SENHA,:TB_INSTITUICAO_COD)";
-            
-                try{
+
+            if ($stmt->rowCount() == 0) {
+                $sql = "INSERT INTO TB_INSTITUICAO (TB_INSTITUICAO_NOME, TB_INSTITUICAO_ENDERECO, TB_INSTITUICAO_END_NUM, TB_INSTITUICAO_CIDADE, TB_INSTITUICAO_CEP, TB_INSTITUICAO_TELEFONE, TB_INSTITUICAO_EMAIL,`TB_INSTITUICAO_SENHA`,TB_INSTITUICAO_COD, TB_INSTITUICAO_IMG_USER) 
+                values(:TB_INSTITUICAO_NOME,:TB_INSTITUICAO_ENDERECO,:TB_INSTITUICAO_END_NUM,:TB_INSTITUICAO_CIDADE,:TB_INSTITUICAO_CEP,:TB_INSTITUICAO_TELEFONE,:TB_INSTITUICAO_EMAIL,:TB_INSTITUICAO_SENHA,:TB_INSTITUICAO_COD, :IMG_INSTITUICAO)";
+
+                try {
                     $handle = $pdo->prepare($sql);
                     $params = [
-                        ':TB_INSTITUICAO_NOME'=>$nome,
-                        ':TB_INSTITUICAO_ENDERECO'=>$endereco,
-                        ':TB_INSTITUICAO_END_NUM'=>$numero,
-                        ':TB_INSTITUICAO_CIDADE'=>$cidade,
-                        ':TB_INSTITUICAO_CEP'=>$cep,
-                        ':TB_INSTITUICAO_TELEFONE'=>$telefone,
-                        ':TB_INSTITUICAO_EMAIL'=>$email,
-                        ':TB_INSTITUICAO_SENHA'=>$senha,
-                        ':TB_INSTITUICAO_COD'=>$codigo
+                        ':TB_INSTITUICAO_NOME' => $nome,
+                        ':TB_INSTITUICAO_ENDERECO' => $endereco,
+                        ':TB_INSTITUICAO_END_NUM' => $numero,
+                        ':TB_INSTITUICAO_CIDADE' => $cidade,
+                        ':TB_INSTITUICAO_CEP' => $cep,
+                        ':TB_INSTITUICAO_TELEFONE' => $telefone,
+                        ':TB_INSTITUICAO_EMAIL' => $email,
+                        ':TB_INSTITUICAO_SENHA' => $senha,
+                        ':TB_INSTITUICAO_COD' => $codigo,
+                        ':IMG_INSTITUICAO' => $textarea
                     ];
-                    
+
                     $handle->execute($params);
-                    
+
                     $success = 'Sua escola está cadastrada na Quíron';
-                    
-                }
-                catch(PDOException $e){
+                } catch (PDOException $e) {
                     $errors[] = $e->getMessage();
                 }
-            }
-            else
-            {
+            } else {
                 $valnome = $nome;
                 $valendereco = $endereco;
                 $valnumero = $numero;
@@ -86,96 +83,66 @@ if(isset($_POST['submit']))
 
                 $errors[] = 'Email já cadastrado';
             }
-        }
-        else
-        {
+        } else {
             $errors[] = "Endereço de email não é válido";
         }
-    }
-    else
-    {
-        if(!isset($_POST['nome-e']) || empty($_POST['nome-e']))
-        {
+    } else {
+        if (!isset($_POST['nome-e']) || empty($_POST['nome-e'])) {
             $errors[] = 'Por favor insira o nome da sua escola';
-        }
-        else
-        {
+        } else {
             $valnome = $_POST['nome-e'];
         }
-        if(!isset($_POST['end-e']) || empty($_POST['end-e']))
-        {
+        if (!isset($_POST['end-e']) || empty($_POST['end-e'])) {
             $errors[] = 'Por favor insira o endereço de sua escola';
-        }
-        else
-        {
+        } else {
             $valendereco = $_POST['end-e'];
         }
 
-        if(!isset($_POST['num-e']) || empty($_POST['num-e']))
-        {
+        if (!isset($_POST['num-e']) || empty($_POST['num-e'])) {
             $errors[] = 'Por favor insira o número do endereço';
-        }
-        else
-        {
+        } else {
             $valendereco = $_POST['num-e'];
         }
 
-        if(!isset($_POST['cidade-e']) || empty($_POST['cidade-e']))
-        {
+        if (!isset($_POST['cidade-e']) || empty($_POST['cidade-e'])) {
             $errors[] = 'Por favor insira a cidade da sua escola';
-        }
-        else
-        {
+        } else {
             $valcidade = $_POST['cidade-e'];
         }
 
-        if(!isset($_POST['cep-e']) || empty($_POST['cep-e']))
-        {
+        if (!isset($_POST['cep-e']) || empty($_POST['cep-e'])) {
             $errors[] = 'Por favor insira o cep da escola';
-        }
-        else
-        {
+        } else {
             $valcep = $_POST['cep-e'];
         }
 
-        if(!isset($_POST['tel-e']) || empty($_POST['tel-e']))
-        {
+        if (!isset($_POST['tel-e']) || empty($_POST['tel-e'])) {
             $errors[] = 'Por favor insira o telefone para contato da escola';
-        }
-        else
-        {
+        } else {
             $valtelefone = $_POST['tel-e'];
         }
 
-        if(!isset($_POST['email-e']) || empty($_POST['email-e']))
-        {
+        if (!isset($_POST['email-e']) || empty($_POST['email-e'])) {
             $errors[] = 'Por favor insira o email para contato da escola';
-        }
-        else
-        {
+        } else {
             $valemail = $_POST['email-e'];
         }
 
-        if(!isset($_POST['senha-e']) || empty($_POST['senha-e']))
-        {
+        if (!isset($_POST['senha-e']) || empty($_POST['senha-e'])) {
             $errors[] = 'Por favor insira a senha';
-        }
-        else
-        {
+        } else {
             $valsenha = $_POST['senha-e'];
         }
 
-        if(!isset($_POST['cod-e']) || empty($_POST['cod-e']))
-        {
+        if (!isset($_POST['cod-e']) || empty($_POST['cod-e'])) {
             $errors[] = 'Por favor insira o código da instituição';
-        }
-        else
-        {
+        } else {
             $valcodigo = $_POST['cod-e'];
         }
 
         
-    }
+            $errors[] = 'Por favor insira o código da ';
+       
 
+    }
 }
-?>
--- src/web/cadastrar_escola.php
+++ src/web/cadastrar_escola.php
@@ -1,12 +1,11 @@
-
-
 <?php
-    include("../server/PDO/conexao.php");
-    include("../server/cad_escola.php");
+include("../server/PDO/conexao.php");
+include("../server/cad_escola.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br" class="light">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,74 +15,75 @@
     <link rel="stylesheet" href="styles/styles_l/editar_info.css">
     <link rel="stylesheet" href="styles/theme.css">
     <script src="scripts/ImagenError.js"></script>
-    
+
     <link rel="icon" href="../web/images/logos/arco-dark-2.png">
-    
+
 </head>
+
 <body>
-<?php 
-    include('../server/PDO/navbar.php'); 
+    <?php
+    include('partials/header-inicio.php'); 
     include('../server/PDO/acesso_telas_desconhecido.php');
-?>
-    <?php 
-				if(isset($errors) && count($errors) > 0)
-				{
-					foreach($errors as $error_msg)
-					{
-						echo '<div class="alert alert-danger">'.$error_msg.'</div>';
-					}
-                }
-                
-                if(isset($success))
-                {
-                    
-                    echo '<div class="alert alert-success">'.$success.'</div>';
-                }
-			?>
-<br><br><br><br>
+    ?>
 
-    <center><h1 class="h1002">Cadastre-se na QUÍRON</h1></center>
+    <br><br><br><br>
+
+    <center>
+        <h1 class="h1002">Cadastre-se na QUÍRON</h1>
+    </center>
+
+    <?php
+    if (isset($errors) && count($errors) > 0) {
+        foreach ($errors as $error_msg) {
+            echo '<div class="alert alert-danger">' . $error_msg . '</div>';
+        }
+    }
+
+    if (isset($success)) {
+
+        echo '<div class="alert alert-success">' . $success . '</div>';
+    }
+    ?>
 
 
-    <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>" enctype="multipart/form-data">
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
 
-    <div class="div-mae">
+        <div class="div-mae">
 
-        <div class="div-alterar-img">
-            <h3>Divulgue sua escola</h3>
+            <div class="div-alterar-img">
+                <h3>Divulgue sua escola</h3>
 
-            <div class="sub-div-alterar-img">
-                <img src="" onerror="handleError(this)" name="imagem" id="preview" class="imagem-anexo" alt="">
+                <div class="sub-div-alterar-img">
+                    <img src="" onerror="handleError(this)" name="imagem" id="preview" class="imagem-anexo" alt="">
+                </div>
+
+                <input type="file" name="foto" class="input-file" id="base">
+                <label id="submit-text-alterar" class="abc001" for="base">Anexar</label>
             </div>
 
-            <input type="file" name="foto" class="input-file" id="base"> 
-            <label id="submit-text-alterar" class="abc001" for="base">Anexar</label>
-        </div>
+            <script src="scripts/mostrar_imagem.js"></script>
 
-        <script src="scripts/mostrar_imagem.js"></script>
+            <div class="div-alterar-texto">
+                <div class="div-alterar-form">
+                    <label class="form-nome-completo aas">Nome da Instituição: </label><input required maxlength="128" name="nome-e" class="input-nome input-text-alterar" type="text"><br>
 
-        <div class="div-alterar-texto">
-            <div class="div-alterar-form">
-            
-                <label class="form-nome">Nome da instituição: </label><input class="input-sozinho" name="nome-e" type="text" maxlength="128" id="input-text-alterar" required><br>
-                
-                <label class="form-endereco">Endereço: </label><input name="end-e" type="text" maxlength="128" class="input-endereco" id="input-text-alterar" required> <label class="form-num"> Nº: </label><input maxlength="6" required name="num-e" class="input-num" type="number" id="input-text-alterar"><br>
-                
-                <label class="form-bairro">Cidade: </label><input name="cidade-e" type="text" maxlength="128" class="input-bairro" id="input-text-alterar" required> <label class="form-cep"> CEP: </label><input name="cep-e" class="input-cep" type="number" id="input-text-alterar" maxlength="8" required><br>
-                
-                <label class="form-tel">Telefone: </label><input type="number" maxlength="11" name="tel-e" class="input-tel" id="input-text-alterar" required> <label class="form-email"> E-mail: </label><input required class="input-email" maxlength="128" name="email-e" type="email" id="input-text-alterar"><br>
+                    <label class="form-endereco aas">Endereço: </label><input required maxlength="128" name="end-e" type="text" class="input-endereco input-text-alterar"> <label class="form-num aas"> Nº: </label><input required oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" maxlength="30" name="num-e" class="input-num input-text-alterar" type="number"><br>
 
-                <center> <label class="form-senha">Senha: </label><input required maxlength="45" type="password" name="senha-e" class="input-senha" id="input-text-alterar"> <label class="form-cod">Código: </label><input maxlength="100" required type="number" class="input-cod" name="cod-e" id="input-text-alterar"></center>
-                
-                <input type="submit" name="submit" value="Cadastrar-se" id="submit-text-alterar" class="submit-class">
-            
+                    <label class="form-bairro aas">Cidade: </label><input required maxlength="128" name="cidade-e" type="text" class="input-bairro input-text-alterar"> <label class="form-cep aas"> CEP: </label><input required oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" maxlength="8" name="cep-e" class="input-cep input-text-alterar" type="number"><br>
+
+                    <label class="form-tel aas">Telefone: </label><input required oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" maxlength="11" name="tel-e" type="number" class="input-tel input-text-alterar"> <label class="form-email aas"> E-mail: </label><input required maxlength="128" name="email-e" class="input-email input-text-alterar" type="email"><br>
+
+                    <center> <label name="senha" class="form-senha aas">Senha: </label><input required maxlength="45" name="senha-e" type="password" class="input-senha input-text-alterar"> <label class="form-cod">Código: </label><input maxlength="100" required type="number" class="input-cod" name="cod-e" id="input-text-alterar"> </center>
+
+                    <input type="submit" name="submit" value="Cadastrar-se" id="submit-text-alterar" class="submit-class">
+                </div>
             </div>
-        </div>
-        
-        <textarea style="display:none" name="imagem"></textarea>
-    </div>
-</form>
 
-<?php include('partials/footer.php') ?>
+            <textarea style="display:none" name="imagem"></textarea>
+        </div>
+    </form>
+
+    <?php include('partials/footer.php') ?>
 </body>
+
 </html>
