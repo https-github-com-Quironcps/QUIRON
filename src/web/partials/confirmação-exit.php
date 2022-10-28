c42bf57fd4cb08cad874c3133d4e0b04f36c47bf
Gabriela5652 <74539725+Gabriela5652@users.noreply.github.com>
2022-10-28 17:30:30 -0300

    divs-confirmacao

--- src/web/partials/confirmação-exit.php
+++ src/web/partials/confirmação-exit.php
@@ -12,24 +12,26 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <script src="scripts/ImagenError.js"></script>
+    <script src="scripts/FechaDiv.js"></script>
 
     <link rel="icon" href="../images/logos/arco-dark-2.png">
 
     <title>QUIRON - CONFIRMAÇÃO</title>
 </head>
 <body>
-
+        <div id="fecharDiv" class="div-todo">
         <div class="principal2">
             <center>
-                <div class="conteudo">
+                <div class="conteudo6">
               
-                        <h3>Deseja Sair?</h3><br>
+                        <h3 class="mensagem">Deseja Sair?</h3><br>
 
                         <input type="submit" value="Sim" class="btn-enviar">
                         <input type="submit" value="Não" class="btn-enviar">
 
                 </div>
             </center>
+        </div>
         </div>
  
 </body>
